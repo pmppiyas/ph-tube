a915630976ce228f704b3baf4fa61fc94d3b93de
Prince Mahmud Piyas <princemahmudpiyas@gmail.com>
2024-10-04 00:14:29 +0600

    load and display by category

--- JavaScript/video.js
+++ JavaScript/video.js
@@ -10,13 +10,13 @@ const loadCaregories = () => {
 //Display Categories
 const displayCategories = categories => {
   const categoriesContainer = document.getElementById('categories');
-
   categories.forEach(element => {
     //get Button
-    const button = document.createElement('button');
-    button.classList = 'btn';
-    button.innerText = element.category;
-    categoriesContainer.append(button);
+    const buttonContainer = document.createElement('div');
+    buttonContainer.innerHTML = `
+    <button onclick="loadCategoryVideos(${element.category_id})" class="btn">${element.category}</button>
+    `;
+    categoriesContainer.append(buttonContainer);
   });
 };
 
@@ -28,6 +28,18 @@ const loadVideos = () => {
     .catch(err => console.log(err));
 };
 
+const loadCategoryVideos = id => {
+  //fetch catagory video
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(res => res.json())
+    .then(data => displayVideos(data.category))
+    .catch(err => console.log(err));
+};
+// "categories": [
+//     {
+//       "category_id": "1001",
+//       "category": "Music"
+//     },
 const cardDemo = {
   category_id: '1003',
   video_id: 'aaai',
@@ -49,16 +61,26 @@ const cardDemo = {
 };
 // Display Videos
 const displayVideos = videos => {
+  const videosContainer = document.getElementById('videos');
+  videosContainer.innerHTML = '';
   videos.forEach(video => {
-    const videosContainer = document.getElementById('videos');
     const card = document.createElement('div');
     card.classList = 'card card-compact ';
     card.innerHTML = `
-      <figure class= "h-[200px]">
+      <figure class= "h-[200px] relative">
     <img
       src="${video.thumbnail}"
       alt="Shoes" class="h-full w-full object-cover" />
+      ${
+        video.others.posted_date?.length == 0
+          ? ''
+          : `<span class="absolute right-2 bottom-2 bg-black text-white text-xs p-1 rounded-md">${getTimeString(
+              video.others.posted_date
+            )}</span>`
+      }
+      
   </figure>
+  
   <div class="py-4 flex gap-2">
   <div class="w-12 h-12">
   <img src="${
@@ -82,6 +104,16 @@ const displayVideos = videos => {
     videosContainer.append(card);
   });
 };
+
+// Time String
+function getTimeString(time) {
+  const hour = parseInt(time / 3600);
+  let reminigSecond = time % 3600;
+  const minute = parseInt(reminigSecond / 60);
+  const second = reminigSecond % 60;
+
+  return `${hour} hour ${minute} minute ${second} second ago`;
+}
 
 loadCaregories();
 loadVideos();
